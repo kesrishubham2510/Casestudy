0f9ada2fe18722ac06d7d9d9e7afaa2fabe65468
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-08 18:53:03 +0530

    * Added placeholders for exception handlers

---
+++ src/main/java/com/myreflectionthoughts/covidstat/handler/ConnectionExceptionHandler.java
@@ -0,0 +1,11 @@
+package com.myreflectionthoughts.covidstat.handler;
+
+import com.myreflectionthoughts.covidstat.contract.IExceptionHandler;
+
+// As, I'm planning to log the exception and serve cached response to client
+public class ConnectionExceptionHandler implements IExceptionHandler<String, Void> {
+    @Override
+    public Void handleException(String input) {
+        return null;
+    }
+}
