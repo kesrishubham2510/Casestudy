5ee4d437393a4914f90847364c997df58af9ae18
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-08 16:17:52 +0530

    init project

---
+++ src/main/java/com/myreflectionthoughts/covidstat/CovidstatApplication.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.covidstat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidstatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidstatApplication.class, args);
+	}
+
+}
