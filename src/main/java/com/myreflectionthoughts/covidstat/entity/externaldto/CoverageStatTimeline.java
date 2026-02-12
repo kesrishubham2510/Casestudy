9bdb6959c98209edd9ee3cb47e918f6a8b272b2a
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-12 20:48:40 +0530

    * Added comment for reference

--- src/main/java/com/myreflectionthoughts/covidstat/entity/externaldto/CoverageStatTimeline.java
+++ src/main/java/com/myreflectionthoughts/covidstat/entity/externaldto/CoverageStatTimeline.java
@@ -2,6 +2,7 @@ package com.myreflectionthoughts.covidstat.entity.externaldto;
 
 import lombok.Data;
 
+// This data tells about only vaccination coverage
 @Data
 public class CoverageStatTimeline {
      private String total;
