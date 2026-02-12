831e493568af4c115093ea1b060a1ef7d9fe7ee6
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-12 20:08:01 +0530

    *  Added | API prefix | API version

--- src/main/java/com/myreflectionthoughts/covidstat/constant/ServiceConstant.java
+++ src/main/java/com/myreflectionthoughts/covidstat/constant/ServiceConstant.java
@@ -10,6 +10,8 @@ public class ServiceConstant {
     public static final String _ERR_OCCURRED_KEY = "_ERR_OCCURRED";
     public static final String _ERR_CACHE_CONNECTION_KEY = "_ERR_CACHE_CONNECTION";
     public static final String _ERR_BAD_REQUEST_KEY = "_ERR_BAD_REQUEST";
+    public static final String API_VERSION = "v1";
+    public static final String API_PREFIX = "/api/covid-stat/";
 
 
 }
