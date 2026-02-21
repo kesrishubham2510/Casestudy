10e47eac8f8dab0cf795e3a423edbe8ec7e787b1
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-02-22 00:19:41 +0530

    Corrected | Annotation

--- src/main/java/com/myreflectionthoughts/covidstat/controller/CovidStatController.java
+++ src/main/java/com/myreflectionthoughts/covidstat/controller/CovidStatController.java
@@ -6,6 +6,7 @@ import com.myreflectionthoughts.covidstat.service.Orchestrator;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController(ServiceConstant.API_PREFIX)
+@RestController
+@RequestMapping(ServiceConstant.API_PREFIX)
 public class CovidStatController {
 
     private final Orchestrator orchestrator;
