0bef9727f3f4c4b7b7f4c61aedd243698ddb03a6
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-04-11 22:59:24 +0530

    Added webclient implementation

---
+++ src/main/java/com/myreflectionthoughts/covidstat/service/WebClientConnection.java
@@ -0,0 +1,96 @@
+package com.myreflectionthoughts.covidstat.service;
+
+import com.myreflectionthoughts.covidstat.constant.ServiceConstant;
+import com.myreflectionthoughts.covidstat.contract.IRemoteConnection;
+import com.myreflectionthoughts.covidstat.exception.CaseStudyException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.net.URI;
+import java.time.Duration;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@Service
+public class WebClientConnection implements IRemoteConnection<String> {
+
+    private final String hostName;
+    private final WebClient webClient;
+    private static final Logger logger;
+
+    static {
+        logger = Logger.getLogger(WebClientConnection.class.getSimpleName());
+    }
+
+    public WebClientConnection() {
+        this.hostName = "https://disease.sh";
+        this.webClient = WebClient.builder()
+                .baseUrl(hostName)
+                .build();
+    }
+
+    @Override
+    public String executeGetRequest(String url, Map<String, String> headers) {
+
+        try {
+            logger.info("Invoking... { " + url + " }");
+
+            String result = webClient.get()
+                    .uri(URI.create(hostName+url))
+                    .headers(httpHeaders -> headers.forEach(httpHeaders::add))
+                    .retrieve()
+                    .onStatus(status -> status.is4xxClientError(), response ->
+                            handleError(response,  ServiceConstant._ERR_BAD_REQUEST_KEY))
+                    .onStatus(status -> status.is5xxServerError(), response ->
+                            handleError(response, ServiceConstant._ERR_REQUEST_PROCESSING_ERROR_KEY))
+                    .bodyToMono(String.class)
+                    .timeout(Duration.ofMillis(2000))
+                    .retryWhen(
+                            Retry.backoff(3, Duration.ofSeconds(1))
+                                    .filter(this::isRetryable)
+                                    .doBeforeRetry(retrySignal -> {
+                                        logger.info("URL:- "+url+" | Retrying :- "+retrySignal.totalRetries()+" | statusCode:- "+", reason:- "+retrySignal.failure().getMessage());
+                                    })
+                    ).block();
+
+            logger.info("Request to 3rd party completed successfully");
+            return result;
+
+        } catch (CaseStudyException e) {
+            throw e;
+        } catch (Exception e) {
+            logger.severe("Exception occurred, while connecting:- " + e.getMessage());
+            throw new CaseStudyException(
+                    ServiceConstant._ERR_CONNECT_KEY,
+                    "Exception | Something Went Wrong while making 3rd party call to " + hostName
+            );
+        }
+    }
+
+    private Mono<CaseStudyException> handleError(
+            ClientResponse response,
+            String errorKey
+    ) {
+        return response.bodyToMono(String.class)
+                .defaultIfEmpty("")
+                .map(body -> {
+                    logger.severe("Exception occurred, status code:- " + response.statusCode().value());
+                    return new CaseStudyException(
+                            errorKey,
+                            response.statusCode().value(),
+                            body
+                    );
+                });
+    }
+
+    private boolean isRetryable(Throwable throwable) {
+        if (throwable instanceof CaseStudyException ex) {
+            logger.severe("Exception occured:- "+ex.getStatusCode()+", message:- "+ex.getMessage());
+            return ex.getStatusCode() >= 500;
+        }
+        return true;
+    }
+}
