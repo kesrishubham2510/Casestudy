c7c13bc742ec993cf64db00058b243b8c2603ba1
kesrishubham2510 <shubhamkeshari90@gmail.com>
2026-04-12 14:55:07 +0530

    Added utility to read data from resource folder Added Tests

--- src/test/java/com/myreflectionthoughts/covidstat/service/RemoteDataSourceTest.java
+++ src/test/java/com/myreflectionthoughts/covidstat/service/RemoteDataSourceTest.java
@@ -1,91 +1,196 @@
 package com.myreflectionthoughts.covidstat.service;
 
-import com.myreflectionthoughts.covidstat.constant.ServiceConstant;
-import com.myreflectionthoughts.covidstat.contract.IDataSource;
+import com.myreflectionthoughts.covidstat.constant.Country;
 import com.myreflectionthoughts.covidstat.contract.IRemoteConnection;
 import com.myreflectionthoughts.covidstat.datasource.RemoteDataSource;
-import com.myreflectionthoughts.covidstat.entity.ResponseWrapper;
-import com.myreflectionthoughts.covidstat.entity.externaldto.ExternalAPIResponse;
-import com.myreflectionthoughts.covidstat.entity.externaldto.LastTwoDaysResponse;
 import com.myreflectionthoughts.covidstat.exception.CaseStudyException;
-import com.myreflectionthoughts.covidstat.registry.URLTemplateRegistry;
-import com.myreflectionthoughts.covidstat.utility.MappingUtility;
+import com.myreflectionthoughts.covidstat.utility.DataUtility;
+import com.myreflectionthoughts.covidstat.utility.TestDataUtility;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-
-import java.net.http.HttpClient;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
-  class RemoteDataSourceTest {
+import com.myreflectionthoughts.covidstat.entity.externaldto.ExternalAPIResponse;
+import com.myreflectionthoughts.covidstat.entity.externaldto.LastTwoDaysResponse;
+import com.myreflectionthoughts.covidstat.exception.FallbackException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
-    private final URLTemplateRegistry urlTemplateRegistry;
+import com.myreflectionthoughts.covidstat.contract.IRemoteConnection;
+import com.myreflectionthoughts.covidstat.entity.externaldto.ExternalAPIResponse;
+import com.myreflectionthoughts.covidstat.entity.externaldto.LastTwoDaysResponse;
+import com.myreflectionthoughts.covidstat.exception.CaseStudyException;
+import com.myreflectionthoughts.covidstat.exception.FallbackException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class RemoteDataSourceTest {
+
     private final IRemoteConnection<String> remoteConnection;
-    private final MappingUtility mappingUtility;
 
-    private final IDataSource<ResponseWrapper> mockRemoteDataSource;
-    private final IRemoteConnection<String> mockRemoteConnection;
+    @InjectMocks
+    private RemoteDataSource remoteDataSource;
 
-    private final RemoteDataSource remoteDataSource;
-    private final String country;
+    private ExternalAPIResponse mockResponse;
 
-     RemoteDataSourceTest(){
-        this.urlTemplateRegistry = URLTemplateRegistry.getURLUrlTemplateRegistryInstance();
-        this.remoteConnection = new HttpConnection(HttpClient.newHttpClient());
-        this.mappingUtility = MappingUtility.getMappingUtilityInstance();
-        this.mockRemoteConnection = Mockito.mock(HttpConnection.class);
-        this.country = "India";
+    public RemoteDataSourceTest(){
+        remoteConnection = Mockito.mock(IRemoteConnection.class);
         this.remoteDataSource = new RemoteDataSource(remoteConnection);
-        this.mockRemoteDataSource = new RemoteDataSource(mockRemoteConnection);
     }
 
     @Test
-     void testGetLatestStats(){
-       ExternalAPIResponse externalAPIResponse = remoteDataSource.getLatestStats(country, 0L);
-       assertEquals(externalAPIResponse.getCountry(), "India");
+    public void testGetLatestStats_Success(){
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn(TestDataUtility.getFileContent("data/LatestCovidStat.json"));
+
+        assertNotNull(remoteDataSource.getLatestStats(Country.INDIA.getDisplayName(), 1));
     }
 
     @Test
-     void testGetLatestStats_Throws_Fails_LatestStat_Parsing(){
-        when(mockRemoteConnection.executeGetRequest(anyString(), anyMap())).thenReturn("{]");
-        CaseStudyException exception = assertThrows(CaseStudyException.class, ()-> mockRemoteDataSource.getLatestStats(country, 0L));
-        assertEquals(ServiceConstant._ERR_PARSING_ERROR_LATEST_STAT_KEY, exception.getKey());
+    public void testGetLatestStats_ThrowsaseStudyException(){
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn("}");
+
+        assertThrows(CaseStudyException.class, ()-> remoteDataSource.getLatestStats(Country.INDIA.getDisplayName(), 1));
     }
 
     @Test
-     void testGetVaccineCoverageForAMonth(){
-        ExternalAPIResponse externalAPIResponse = remoteDataSource.getVaccineCoverage(country, 0L);
-        assertEquals(externalAPIResponse.getTimeline().size(), 30);
+    public void testGetVaccineCoverage_Success() {
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn(TestDataUtility.getFileContent("data/VaccineCoverage_India_Last_25_Days.json"));
+
+        ExternalAPIResponse response =
+                remoteDataSource.getVaccineCoverage(Country.INDIA.getDisplayName(), 10);
+
+        assertNotNull(response);
     }
 
     @Test
-     void testGetLatestStats_Throws_Fails_VaccineCoverage_Parsing(){
-        when(mockRemoteConnection.executeGetRequest(anyString(), anyMap())).thenReturn("{]");
-        CaseStudyException exception = assertThrows(CaseStudyException.class, ()-> mockRemoteDataSource.getVaccineCoverage(country, 0L));
-        assertEquals(ServiceConstant._ERR_PARSING_ERROR_VACCINE_COVERAGE_KEY, exception.getKey());
+    public void testGetVaccineCoverage_ThrowsCaseStudyException() {
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn("}");
+
+        assertThrows(CaseStudyException.class,
+                     () -> remoteDataSource.getVaccineCoverage(Country.INDIA.getDisplayName(), 10));
     }
 
     @Test
-     void testGetVaccineCoverageForLast3Days(){
-        ExternalAPIResponse externalAPIResponse = remoteDataSource.getVaccineCoverage(country, 3L);
-        assertEquals(externalAPIResponse.getTimeline().size(), 3);
+    public void testGetVaccineCoverage_Global_Success() {
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn(TestDataUtility.getFileContent("data/VaccineCoverage_Global_Last_25_Days.json"));
+
+        ExternalAPIResponse response =
+                remoteDataSource.getVaccineCoverage("global", 10);
+
+        assertNotNull(response);
     }
 
     @Test
-     void testGetDataForAlerts(){
-        LastTwoDaysResponse lastTwoDaysResponse = remoteDataSource.getDataForAlerts(country, 0L);
-        assertNotEquals(lastTwoDaysResponse.getLastTwoDaysResponse().get(0).getUpdated(), lastTwoDaysResponse.getLastTwoDaysResponse().get(1).getUpdated());
+    public void testGetDataForAlerts_Success() {
+
+        String json = TestDataUtility.getFileContent("data/LatestCovidStat.json");
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn(json);
+
+        LastTwoDaysResponse response =
+                remoteDataSource.getDataForAlerts(Country.INDIA.getDisplayName(), 1);
+
+        assertNotNull(response);
+        assertEquals(2, response.getLastTwoDaysResponse().size());
     }
 
     @Test
-     void testGetLatestStats_Throws_Fails_DailyAlert_Parsing(){
-        when(mockRemoteConnection.executeGetRequest(anyString(), anyMap())).thenReturn("{]");
-        CaseStudyException exception = assertThrows(CaseStudyException.class, ()-> mockRemoteDataSource.getDataForAlerts(country, 0L));
-        assertEquals(ServiceConstant._ERR_PARSING_ERROR_DAILY_STAT_KEY, exception.getKey());
+    public void testGetDataForAlerts_FirstCallParsingFails() {
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn("}");
+        assertThrows(CaseStudyException.class,
+                     () -> remoteDataSource.getDataForAlerts(Country.INDIA.getDisplayName(), 1));
     }
 
+    @Test
+    public void testGetDataForAlerts_SecondCallParsingFails() {
+
+        String validJson = TestDataUtility.getFileContent("data/LatestCovidStat.json");
+
+        when(remoteConnection.executeGetRequest(anyString(), anyMap()))
+                .thenReturn(validJson)
+                .thenReturn("}");
+
+        assertThrows(CaseStudyException.class,
+                     () -> remoteDataSource.getDataForAlerts(Country.INDIA.getDisplayName(), 1));
+    }
+
+    @Test
+    public void testStaticCountryStats_Success() {
+
+        ExternalAPIResponse response =
+                remoteDataSource.staticCountryStats(Country.AFGHANISTAN.getDisplayName(), 0);
+
+        assertNotNull(response);
+        assertTrue(response.isServedFromCache());
+        assertEquals(Country.AFGHANISTAN.getDisplayName(), response.getCountry());
+    }
+
+    @Test
+    public void testStaticCountryStats_ThrowsFallbackException() {
+
+        assertThrows(FallbackException.class,
+                     () -> remoteDataSource.staticCountryStats(Country.INDIA.getDisplayName(), 0));
+    }
+
+    @Test
+    public void testStaticVaccineCoverageStats_Success() {
+
+        ExternalAPIResponse response =
+                remoteDataSource.staticVaccineCoverageStats(Country.AFGHANISTAN.getDisplayName(), 0);
+
+        assertNotNull(response);
+        assertTrue(response.isServedFromCache());
+        assertEquals(Country.AFGHANISTAN.getDisplayName(), response.getCountry());
+    }
+
+    @Test
+    public void testStaticVaccineCoverageStats_ThrowsFallbackException() {
+
+        assertThrows(FallbackException.class,
+                     () -> remoteDataSource.staticVaccineCoverageStats(Country.IRELAND.getDisplayName(), 0));
+    }
+
+    @Test
+    public void testStaticLastTwoDayStats_Success() {
+
+        LastTwoDaysResponse response =
+                remoteDataSource.staticLastTwoDayStats(Country.AFGHANISTAN.getDisplayName(), 0);
+
+        assertNotNull(response);
+        assertEquals(2, response.getLastTwoDaysResponse().size());
+
+        response.getLastTwoDaysResponse()
+                .forEach(r -> assertTrue(r.isServedFromCache()));
+    }
+
+    @Test
+    public void testStaticLastTwoDayStats_ThrowsFallbackException() {
+
+        assertThrows(FallbackException.class,
+                     () -> remoteDataSource.staticLastTwoDayStats(Country.DRC.getDisplayName(), 0));
+    }
 
 }
--- src/test/java/com/myreflectionthoughts/covidstat/utility/TestDataUtility.java
+++ src/test/java/com/myreflectionthoughts/covidstat/utility/TestDataUtility.java
@@ -2,22 +2,15 @@ package com.myreflectionthoughts.covidstat.utility;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.myreflectionthoughts.covidstat.entity.CovidStatResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
-public class DataUtility {
+public class TestDataUtility {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    private DataUtility(){
-
-    }
     public static <T> T convertTOPOJO(String content, Class<T> t) {
         try {
             return objectMapper.readValue(content, t);
@@ -29,7 +22,7 @@ public class DataUtility {
     public static String getFileContent(String filePath){
 
         try {
-            InputStream is = DataUtility.class
+            InputStream is = TestDataUtility.class
                     .getClassLoader()
                     .getResourceAsStream(filePath);
 
@@ -42,17 +35,6 @@ public class DataUtility {
             throw new RuntimeException(e);
         }
     }
-
-    public static long calculateTheDaysBack(String referencedDate){
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-        LocalDate givenDate = LocalDate.parse(referencedDate, formatter);
-        LocalDate today = LocalDate.now();
-
-        return ChronoUnit.DAYS.between(givenDate, today);
-    }
-
     public static <T> T getDefaultResponse(String path, Class<T> t){
         String content = getFileContent(path);
         return convertTOPOJO(content, t);
